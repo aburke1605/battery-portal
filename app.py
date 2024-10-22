e10db109707a07984edb5a6f3b6916c0f7bbbe8f
aburke1605 <aodhanburke@hotmail.com>
2024-10-22 11:58:52 +0100

    add a plot as proof of principle

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
-from flask import Flask
+from flask import Flask, Response, render_template, url_for
+
+import io
+import random
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
-from flask import url_for
-
 @app.route('/')
 def index():
-    return 'index'
+   print('Request for index page received')
+   return render_template('index.html')
 
-@app.route('/login')
-def login():
-    return 'login'
 
-@app.route('/user/<username>')
-def profile(username):
-    return f'{username}\'s profile'
+def create_figure():
+    fig, ax = plt.subplots()
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    ax.plot(xs, ys)
+    return fig
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
 
 with app.test_request_context():
+    print(url_for('static', filename='style.css'))
     print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='John Doe'))
+    print(url_for('plot_png'))
+
+
+if __name__ == '__main__':
+   app.run()
