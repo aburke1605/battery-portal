bfbe1dc5cbe77bf778c0f654f8d0be981b62bfb2
aburke1605 <aodhanburke@hotmail.com>
2024-11-14 13:35:46 +0000

    move AP function to another file

--- main/battery-portal.c
+++ main/battery-portal.c
@@ -6,53 +6,10 @@
 
 #include "src/DNS.c"
 #include "src/I2C.c"
+#include "src/AP.h"
 #include "src/handlers.h"
 
-#define WIFI_SSID "ESP32-AP"
-#define WIFI_PASS ""//"12345678"
-#define MAX_STA_CONN 4
-
 static int CONFIG_MAX_CLIENTS = 5;
-
-void wifi_init_softap(void) {
-    // Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
-
-    // Initialize the Wi-Fi stack
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_ap();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    // Configure the Access Point
-    wifi_config_t wifi_config = {
-        .ap = {
-            .ssid = WIFI_SSID,
-            .ssid_len = strlen(WIFI_SSID),
-            .channel = 1,
-            .password = WIFI_PASS,
-            .max_connection = MAX_STA_CONN,
-            .authmode = WIFI_AUTH_WPA_WPA2_PSK
-        },
-    };
-
-    if (strlen(WIFI_PASS) == 0) {
-        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
-    }
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP)); // Set ESP32 as AP
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_LOGI("test", "WiFi AP started. SSID: %s, Password: %s", WIFI_SSID, WIFI_PASS);
-}
 
 /*
  * Structure holding server handle
