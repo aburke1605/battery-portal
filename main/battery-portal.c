ba3ebbcbaddee9f9a01c8f6ff0a553134a183223
aburke1605 <aodhanburke@hotmail.com>
2024-11-22 10:52:10 +0000

    this does something

--- main/battery-portal.c
+++ main/battery-portal.c
@@ -1,12 +1,85 @@
 #include <esp_spiffs.h>
+#include <esp_wifi.h>
+#include <esp_websocket_client.h>
 
 #include "include/AP.h"
 #include "include/DNS.h"
 #include "include/I2C.h"
 #include "include/WS.h"
 
+#define WEBSOCKET_URI "ws://192.168.4.1:80/ws" // Replace with the WebSocket server URI
+
 // Define the server globally
 httpd_handle_t server = NULL;
+
+void wifi_init_sta(void) {
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = "AceOn battery",
+            .password = "password",
+        },
+    };
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config);
+    esp_wifi_start();
+    esp_wifi_connect();
+}
+
+static void websocket_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
+    esp_websocket_event_data_t *data = (esp_websocket_event_data_t *)event_data;
+
+    switch (event_id) {
+        case WEBSOCKET_EVENT_CONNECTED:
+            ESP_LOGI("battery-portal", "WebSocket connected");
+            break;
+
+        case WEBSOCKET_EVENT_DISCONNECTED:
+            ESP_LOGI("battery-portal", "WebSocket disconnected");
+            break;
+
+        case WEBSOCKET_EVENT_DATA:
+            ESP_LOGI("battery-portal", "WebSocket data received: %.*s", data->data_len, (char *)data->data_ptr);
+            break;
+
+        case WEBSOCKET_EVENT_ERROR:
+            ESP_LOGE("battery-portal", "WebSocket error occurred");
+            break;
+
+        default:
+            ESP_LOGW("battery-portal", "Unhandled WebSocket event ID: %ld", event_id);
+            break;
+    }
+}
+
+void websocket_client_task(void *pvParameters) {
+    // Configure WebSocket client
+    esp_websocket_client_config_t websocket_cfg = {
+        .uri = WEBSOCKET_URI,
+    };
+
+    // Initialize WebSocket client
+    esp_websocket_client_handle_t client = esp_websocket_client_init(&websocket_cfg);
+    esp_websocket_register_events(client, ESP_EVENT_ANY_ID, websocket_event_handler, NULL);
+
+    // Start WebSocket client
+    esp_websocket_client_start(client);
+
+    // Keep the task running indefinitely
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(1000)); // Add a delay to avoid task starvation
+    }
+
+    // Cleanup (this point will not be reached in the current loop)
+    esp_websocket_client_stop(client);
+    esp_websocket_client_destroy(client);
+    vTaskDelete(NULL);
+}
 
 void app_main(void) {
     // initialise SPIFFS
@@ -37,6 +110,8 @@ void app_main(void) {
 
     // Start the Access Point
     wifi_init_softap();
+    wifi_init_sta();
+    vTaskDelay(pdMS_TO_TICKS(3000));
 
     // Start the ws server to serve the HTML page
     // static httpd_handle_t server = NULL;
@@ -50,4 +125,7 @@ void app_main(void) {
     xTaskCreate(&dns_server_task, "dns_server_task", 4096, NULL, 5, NULL);
 
     xTaskCreate(&websocket_broadcast_task, "websocket_broadcast_task", 4096, &server, 5, NULL);
+
+    // Start WebSocket Client to connect to another ESP32
+    xTaskCreate(websocket_client_task, "websocket_client_task", 4096, NULL, 5, NULL);
 }
