ee7df134e521d647d439ad636daa6a0e58d5d331
aburke1605 <aodhanburke@hotmail.com>
2024-11-26 15:17:56 +0000

    I think all these things are needed

--- main/src/AP.c
+++ main/src/AP.c
@@ -6,20 +6,6 @@
 
 #include "include/AP.h"
 
-void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
-    if (event_base == WIFI_EVENT) {
-        if (event_id == WIFI_EVENT_STA_START) {
-            esp_wifi_connect();
-        } else if (event_id == WIFI_EVENT_STA_DISCONNECTED) {
-            ESP_LOGI("AP", "Disconnected from STA, retrying...");
-            esp_wifi_connect();
-        }
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
-        ESP_LOGI("AP", "Got IP: " IPSTR, IP2STR(&event->ip_info.ip));
-    }
-}
-
 void wifi_init(void) {
     // Initialize NVS
     esp_err_t ret = nvs_flash_init();
@@ -31,16 +17,13 @@ void wifi_init(void) {
     // Initialize the Wi-Fi stack
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    esp_netif_create_default_wifi_ap();
+    
     esp_netif_create_default_wifi_sta();
+    esp_netif_create_default_wifi_ap();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL, NULL));
-    
     // Set Wi-Fi mode to both AP and STA
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
 
@@ -69,8 +52,9 @@ void wifi_init(void) {
     };
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_sta_config));
 
+    ESP_LOGI("AP", "Starting WiFi AP... SSID: %s, Password: %s", WIFI_SSID, WIFI_PASS);
     ESP_ERROR_CHECK(esp_wifi_start());
 
-    ESP_LOGI("AP", "WiFi AP started. SSID: %s, Password: %s", WIFI_SSID, WIFI_PASS);
-    ESP_LOGI("AP", "Connecting to SSID: %s", wifi_sta_config.sta.ssid);
+    ESP_LOGI("AP", "Connecting to AP... SSID: %s", wifi_sta_config.sta.ssid);
+    ESP_ERROR_CHECK(esp_wifi_connect());
 }
