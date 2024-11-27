b177df6de57ca780eaaa508300be603030ae8700
aburke1605 <aodhanburke@hotmail.com>
2024-11-27 12:06:52 +0000

    hurray! manually change the IP addresses so that the ESP can both send and receive websocket data

--- main/src/AP.c
+++ main/src/AP.c
@@ -19,13 +19,13 @@ void wifi_init(void) {
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     
     esp_netif_create_default_wifi_sta();
-    esp_netif_create_default_wifi_ap();
+    esp_netif_t *ap_netif = esp_netif_create_default_wifi_ap();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
     // Set Wi-Fi mode to both AP and STA
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
 
     // Configure the Access Point
     wifi_config_t wifi_ap_config = {
@@ -41,7 +41,31 @@ void wifi_init(void) {
     if (strlen(WIFI_PASS) == 0) {
         wifi_ap_config.ap.authmode = WIFI_AUTH_OPEN;
     }
-    // ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_ap_config));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_ap_config));
+
+
+    // manually set the netmask and gateway as something different from first ESP
+    // TODO: can we somehow 'discover' the available APs and their IPs,
+    //       and choose a unique one here?
+    ESP_ERROR_CHECK(esp_netif_dhcps_stop(ap_netif));
+    esp_ip4_addr_t ip_info = {
+        .addr = esp_ip4addr_aton("192.168.5.1"),
+    };
+    esp_ip4_addr_t netmask = {
+        .addr = esp_ip4addr_aton("255.255.255.0"),
+    };
+
+    esp_ip4_addr_t gateway = {
+        .addr = esp_ip4addr_aton("192.168.5.1"),
+    };
+    esp_netif_ip_info_t ip_info_struct;
+    ip_info_struct.ip = ip_info;
+    ip_info_struct.netmask = netmask;
+    ip_info_struct.gw = gateway;
+    ESP_ERROR_CHECK(esp_netif_set_ip_info(ap_netif, &ip_info_struct));
+    ESP_ERROR_CHECK(esp_netif_dhcps_start(ap_netif));
+    ESP_LOGI("wifi_init_ap", "AP initialized with IP: 192.168.5.1");
+
 
     wifi_config_t wifi_sta_config = {
         .sta = {
