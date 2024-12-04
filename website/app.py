e078d95ed19c9c626ae93219406c6385b2a23147
zyf <zhaoyunfeng@qiyi.com>
2024-12-04 17:12:32 +0000

    Update app.py

--- website/app.py
+++ website/app.py
@@ -1,6 +1,8 @@
 #!venv/bin/python
 import os
+import string
 from datetime import time, datetime
+import random
 
 from flask import Flask, url_for, redirect, render_template, request, abort
 from flask_sqlalchemy import SQLAlchemy
@@ -13,8 +15,6 @@ from flask_admin import helpers as admin_helpers
 from flask_socketio import SocketIO, emit
 import time
 from wtforms import PasswordField
-from flask_login import logout_user
-from flask_security import url_for_security
 
 # Create Flask application
 app = Flask(__name__)
@@ -171,6 +171,28 @@ def build_sample_db():
             password=hash_password('admin'),
             roles=[user_role, super_user_role]
         )
+        first_names = [
+            'Harry', 'Amelia', 'Oliver', 'Jack', 'Isabella', 'Charlie', 'Sophie', 'Mia',
+            'Jacob', 'Thomas', 'Emily', 'Lily', 'Ava', 'Isla', 'Alfie', 'Olivia', 'Jessica',
+            'Riley', 'William', 'James', 'Geoffrey', 'Lisa', 'Benjamin', 'Stacey', 'Lucy'
+        ]
+        last_names = [
+            'Brown', 'Smith', 'Patel', 'Jones', 'Williams', 'Johnson', 'Taylor', 'Thomas',
+            'Roberts', 'Khan', 'Lewis', 'Jackson', 'Clarke', 'James', 'Phillips', 'Wilson',
+            'Ali', 'Mason', 'Mitchell', 'Rose', 'Davis', 'Davies', 'Rodriguez', 'Cox', 'Alexander'
+        ]
+
+        for i in range(len(first_names)):
+            tmp_email = first_names[i].lower() + "." + last_names[i].lower() + "@example.com"
+            tmp_pass = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(10))
+            user_datastore.create_user(
+                first_name=first_names[i],
+                last_name=last_names[i],
+                email=tmp_email,
+                password=hash_password(tmp_pass),
+                roles=[user_role, ]
+            )
+
         db.session.commit()
     return
 
@@ -180,4 +202,4 @@ if __name__ == '__main__':
     if not os.path.exists(database_path):
         build_sample_db()
     # Start app
-    app.run(debug=True)
+    app.run(debug=False)
