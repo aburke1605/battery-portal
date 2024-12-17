f82b6fcf6189d12caef1826b53e16ec3fe5b78a0
aburke1605 <aodhanburke@hotmail.com>
2024-12-17 14:54:33 +0000

    add configurable data to website send task as well

--- main/src/WS.c
+++ main/src/WS.c
@@ -625,12 +625,26 @@ void website_send_task(void *pvParameters) {
         uint16_t iTemperature = read_2byte_data(TEMPERATURE_REG);
         float fTemperature = (float)iTemperature / 10.0 - 273.15;
 
+        // configurable data
+        uint16_t iBL = test_read(DISCHARGE_SUBCLASS_ID, BL_OFFSET);
+        uint16_t iBH = test_read(DISCHARGE_SUBCLASS_ID, BH_OFFSET);
+        uint16_t iCCT = test_read(CURRENT_THRESHOLDS_SUBCLASS_ID, CHG_CURRENT_THRESHOLD_OFFSET);
+        uint16_t iDCT = test_read(CURRENT_THRESHOLDS_SUBCLASS_ID, DSG_CURRENT_THRESHOLD_OFFSET);
+        uint16_t iCITL = test_read(CHARGE_INHIBIT_CFG_SUBCLASS_ID, CHG_INHIBIT_TEMP_LOW_OFFSET);
+        uint16_t iCITH = test_read(CHARGE_INHIBIT_CFG_SUBCLASS_ID, CHG_INHIBIT_TEMP_HIGH_OFFSET);
+
         // Create JSON object with sensor data
         cJSON *json = cJSON_CreateObject();
         cJSON_AddNumberToObject(json, "charge", iCharge);
         cJSON_AddNumberToObject(json, "voltage", fVoltage);
         cJSON_AddNumberToObject(json, "current", fCurrent);
         cJSON_AddNumberToObject(json, "temperature", fTemperature);
+        cJSON_AddNumberToObject(json, "BL", iBL);
+        cJSON_AddNumberToObject(json, "BH", iBH);
+        cJSON_AddNumberToObject(json, "CCT", iCCT);
+        cJSON_AddNumberToObject(json, "DCT", iDCT);
+        cJSON_AddNumberToObject(json, "CITL", iCITL);
+        cJSON_AddNumberToObject(json, "CITH", iCITH);
 
         char *json_string = cJSON_PrintUnformatted(json);
         cJSON_Delete(json);
