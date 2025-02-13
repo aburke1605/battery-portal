7f0b7cf793e008d3be69b0825fa7f41afb18b3b1
aburke1605 <aodhanburke@hotmail.com>
2025-02-13 10:34:44 +0000

    replace scan function with older ping code

--- main/include/ARP.h
+++ main/include/ARP.h
@@ -1,11 +1,13 @@
 #include <lwip/ip4_addr.h>
 
 extern bool connected_to_WiFi;
+extern char successful_ips[256][16];
+extern char old_successful_ips[256][16];
+extern uint8_t successful_ip_count;
+extern uint8_t old_successful_ip_count;
 extern char ESP_IP[16];
 
 void send_arp_request(ip4_addr_t target_ip);
-
-void scan_network(const char *subnet);
 
 void print_arp_table();
 
--- main/src/ARP.c
+++ main/src/ARP.c
@@ -19,19 +19,6 @@ void send_arp_request(ip4_addr_t target_ip) {
     }
 }
 
-void scan_network(const char *subnet) {
-    ip4_addr_t target_ip;
-    char ip_str[16];
-
-    for (int i = 1; i < 255; i++) {  // Scan 192.168.x.1 - 192.168.x.254
-        snprintf(ip_str, sizeof(ip_str), "%s.%d", subnet, i);
-        if (ip4addr_aton(ip_str, &target_ip)) {
-            send_arp_request(target_ip);
-        }
-        vTaskDelay(pdMS_TO_TICKS(10));  // Small delay to avoid flooding
-    }
-}
-
 void print_arp_table() {
     struct eth_addr *eth_ret;
     struct netif *netif_ret;
@@ -45,6 +32,7 @@ void print_arp_table() {
                 IP2STR(ip_ret),
                 eth_ret->addr[0], eth_ret->addr[1], eth_ret->addr[2],
                 eth_ret->addr[3], eth_ret->addr[4], eth_ret->addr[5]);
+                snprintf(successful_ips[successful_ip_count++], sizeof(successful_ips[0]), IPSTR, IP2STR(ip_ret));
         }
     }
 }
@@ -52,8 +40,35 @@ void print_arp_table() {
 void get_devices_task(void *pvParameters) {
     while (true) {
         if (connected_to_WiFi) {
-            scan_network("192.168.137");
-            vTaskDelay(pdMS_TO_TICKS(3000));  // Wait for ARP replies
+            // get and set the IP address of the ESP32
+            esp_netif_t *sta_netif = esp_netif_get_handle_from_ifkey("WIFI_STA_DEF");
+            if (sta_netif == NULL) return;
+            esp_netif_ip_info_t ip_info;
+            esp_netif_get_ip_info(sta_netif, &ip_info);
+            esp_ip4addr_ntoa(&ip_info.ip, ESP_IP, 16);
+
+
+            uint32_t network_addr = ip_info.ip.addr & ip_info.netmask.addr; // network address
+            uint32_t broadcast_addr = network_addr | ~ip_info.netmask.addr; // broadcast address
+
+            // save successful IPs and reset for next iteration
+            old_successful_ip_count = successful_ip_count;
+            for (size_t i = 0; i < old_successful_ip_count; i++) strcpy(old_successful_ips[i], successful_ips[i]);
+            successful_ip_count = 0;
+
+            // scan the network
+            uint32_t current_ip = network_addr + htonl(1); // start with the first usable IP
+            while (current_ip < broadcast_addr) {
+                ip4_addr_t target_ip;
+                ip4_addr_set_u32(&target_ip, current_ip);
+                send_arp_request(target_ip);
+                current_ip = htonl(ntohl(current_ip) + 1);
+                vTaskDelay(pdMS_TO_TICKS(10)); // small delay to avoid flooding
+            }
+
+            // wait for ARP replies
+            vTaskDelay(pdMS_TO_TICKS(3000));
+
             print_arp_table();
         }
         vTaskDelay(pdMS_TO_TICKS(5000));  // Print every 5 seconds
