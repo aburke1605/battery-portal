9ebe4e92e1fb0e08d6fb8a66261c81aae4dc8f43
aburke1605 <aodhanburke@hotmail.com>
2025-02-20 12:05:14 +0000

    add config for eduroam username and password

--- main/src/utils.c
+++ main/src/utils.c
@@ -5,6 +5,9 @@
 
 #include <esp_random.h>
 
+#define EDUROAM_USERNAME CONFIG_EDUROAM_USERNAME
+#define EDUROAM_PASSWORD CONFIG_EDUROAM_PASSWORD
+
 void random_key(char *key) {
     const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     size_t charset_size = sizeof(charset) - 1;
@@ -24,7 +27,8 @@ void send_fake_post_request() {
 
         esp_http_client_handle_t client = esp_http_client_init(&config);
 
-        const char *post_data = "ssid=<USERNAME>@liverpool.ac.uk&password=<PASSWORD>";
+        char post_data[128];
+        snprintf(post_data, sizeof(post_data), "ssid=%s@liverpool.ac.uk&password=%s", EDUROAM_USERNAME, EDUROAM_PASSWORD);
         esp_http_client_set_header(client, "Content-Type", "application/x-www-form-urlencoded");
         esp_http_client_set_post_field(client, post_data, strlen(post_data));
 
