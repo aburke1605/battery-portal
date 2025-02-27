7811ddc7abbc8f1ee03a114079ca432cce5d36ca
aburke1605 <aodhanburke@hotmail.com>
2025-02-27 16:20:11 +0000

    oops bad syntax

--- main/src/WS.c
+++ main/src/WS.c
@@ -1141,7 +1141,7 @@ void websocket_task(void *pvParameters) {
                     .uri = uri,
                     .reconnect_timeout_ms = 10000,
                     .network_timeout_ms = 10000,
-                    .cert_pem = (const char *)DEV?local_cert_pem:website_cert_pem,
+                    .cert_pem = DEV?(const char *)local_cert_pem:(const char *)website_cert_pem,
                     .skip_cert_common_name_check = DEV,
                 };
 
