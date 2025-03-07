b5b2b7fcdf29fbccb39a1754bb915bc50e532113
aburke1605 <aodhanburke@hotmail.com>
2025-03-07 16:36:04 +0000

    add plot

--- website/app.py
+++ website/app.py
@@ -2,7 +2,13 @@
 
 import mysql.connector
 from flask import Flask, render_template
+
 import time
+
+import matplotlib.pyplot as plt
+import io
+import base64
+
 
 app = Flask(__name__)
 
@@ -56,17 +62,15 @@ try:
         print("table created")
 
     else:
-        cursor.execute(f"""
-                                    SELECT COUNT(*) FROM {esp_id}
-        """)
+        cursor.execute(f"       SELECT COUNT(*) FROM {esp_id}")
         n_rows = cursor.fetchone()[0]
         print("table already exists")
 
 
     while n_rows < 10:
         cursor.execute(f"""
-                                    INSERT INTO {esp_id} (timestamp, soc, temperature, voltage, current)
-                                    VALUES (FROM_UNIXTIME({time.time()}), {31}, {5.5}, {3.7}, {0.1})
+                                INSERT INTO {esp_id} (timestamp, soc, temperature, voltage, current)
+                                VALUES (FROM_UNIXTIME({time.time()}), {31}, {5.5}, {3.7}, {0.1})
         """)
         db.commit()
 
@@ -74,15 +78,25 @@ try:
         time.sleep(1)
 
 
-    cursor.execute(f"""
-                                SELECT * FROM {esp_id}
-    """)
+    cursor.execute(f"           SELECT * FROM {esp_id}")
     table = cursor.fetchall()
 
 except mysql.connector.Error as err:
     print(f"Error: {err}")
 
 
+
+fig, ax = plt.subplots()
+cursor.execute(f"               SELECT timestamp FROM {esp_id}"); t = cursor.fetchall()
+cursor.execute(f"               SELECT soc FROM {esp_id}"); y = cursor.fetchall()
+ax.plot(t, y)
+ax.set_xlabel("timestamp")
+ax.set_ylabel("soc [%]")
+plt.gcf().autofmt_xdate()
+img = io.BytesIO()
+fig.savefig(img, format="png", bbox_inches='tight')
+# img.seek(0) # rewind the buffer (needed?)
+
 @app.route('/')
 def index():
-    return render_template("test.html", table=table)
+    return render_template("test.html", table=table, plot_url=base64.b64encode(img.getvalue()).decode())
