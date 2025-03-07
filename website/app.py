fa45249dc2b6f685bfb05dd898ebef62cc796d0b
aburke1605 <aodhanburke@hotmail.com>
2025-03-07 16:10:46 +0000

    some actual SQL!

--- website/app.py
+++ website/app.py
@@ -2,42 +2,87 @@
 
 import mysql.connector
 from flask import Flask, render_template
+import time
 
 app = Flask(__name__)
-db = mysql.connector.connect(
-    host="batteryportal-server.mysql.database.azure.com",
-    user="hroolfgemh",
-    password="Qaz123ws",
-    database="batteryportal-database",
-    port=3306,
-    ssl_ca="DigiCertGlobalRootCA.crt.pem",
-    ssl_disabled=False
-)
+
+local = False
+if local:
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password",
+        database="battery_data",
+    )
+else:
+    db = mysql.connector.connect(
+        host="batteryportal-server.mysql.database.azure.com",
+        user="hroolfgemh",
+        password="Qaz123ws",
+        database="batteryportal-database",
+        port=3306,
+        ssl_ca="DigiCertGlobalRootCA.crt.pem",
+        ssl_disabled=False
+    )
+
 
 cursor = db.cursor()
-cursor.execute("SHOW TABLES")
-data = "none"
-n = 0
-for table in cursor.fetchall():
-    data = table
-    n += 1
 
-if n == 0:
-    cursor.execute("""
-        CREATE TABLE users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(255),
-            email VARCHAR(255)
-        )
+table = """
+    <----->
+     EMPTY
+    <----->
+"""
+
+esp_id = "BMS_01"
+
+try:
+    n_rows = 0
+
+    cursor.execute(f"""
+                                SHOW TABLES LIKE '{esp_id}'
     """)
-    db.commit() # commit the transaction to save changes
-    print("Table 'users' created.")
+    if not cursor.fetchone():
+        cursor.execute(f"""
+                                CREATE TABLE {esp_id} (
+                                    timestamp TIMESTAMP PRIMARY KEY,
+                                    soc INT,
+                                    temperature FLOAT,
+                                    voltage FLOAT,
+                                    current FLOAT
+                                )
+        """)
+        db.commit()
+        print("table created")
 
-cursor.execute("SHOW TABLES")
-for table in cursor.fetchall():
-    data = table
-    n += 1
+    else:
+        cursor.execute(f"""
+                                    SELECT COUNT(*) FROM {esp_id}
+        """)
+        n_rows = cursor.fetchone()[0]
+        print("table already exists")
+
+
+    while n_rows < 10:
+        cursor.execute(f"""
+                                    INSERT INTO {esp_id} (timestamp, soc, temperature, voltage, current)
+                                    VALUES (FROM_UNIXTIME({time.time()}), {31}, {5.5}, {3.7}, {0.1})
+        """)
+        db.commit()
+
+        n_rows += 1
+        time.sleep(1)
+
+
+    cursor.execute(f"""
+                                SELECT * FROM {esp_id}
+    """)
+    table = cursor.fetchall()
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+
 
 @app.route('/')
 def index():
-    return render_template("test.html", n=n, data=data)
+    return render_template("test.html", table=table)
