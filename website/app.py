c30826962045bc7b39df2738eebc42c2fc88c7cc
aburke1605 <aodhanburke@hotmail.com>
2025-03-07 11:55:53 +0000

    instantiate an app

--- website/app.py
+++ website/app.py
@@ -1,4 +1,10 @@
+#!venv/bin/python
+
 import mysql.connector
+from flask import Flask
+import time
+
+app = Flask(__name__)
 
 db = mysql.connector.connect(
     host="batteryportal-server.mysql.database.azure.com",
@@ -6,8 +12,13 @@ db = mysql.connector.connect(
     password="Qaz123ws",
     database="batteryportal-database"
 )
-
 cursor = db.cursor()
-cursor.execute("SHOW TABLES")
-for table in cursor.fetchall():
-    print(table)
+
+@app.route('/')
+def index():
+    while True:
+        cursor.execute("SHOW TABLES")
+        for table in cursor.fetchall():
+            print(table)
+        
+        time.sleep(5)
