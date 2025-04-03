8d573ff1396ce1870bb592d4620b6c928b9db2f8
aburke1605 <aodhanburke@hotmail.com>
2025-04-03 13:30:40 +0100

    unnecessary comment

--- website/frontend/src/components/charts/DataChart.tsx
+++ website/frontend/src/components/charts/DataChart.tsx
@@ -17,7 +17,7 @@ const formatTimestamp = (timestamp: string) => {
     hour: "2-digit",
     minute: "2-digit",
     second: "2-digit",
-    hour12: false, // forces 24-hour format
+    hour12: false,
     timeZone: "UTC",
     timeZoneName: "short",
   });
