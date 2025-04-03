6c8b6ecbb550a3b0813a4140ce8ec7e179598f71
aburke1605 <aodhanburke@hotmail.com>
2025-04-03 09:38:34 +0100

    change axes labels

--- website/frontend/src/components/charts/DataChart.tsx
+++ website/frontend/src/components/charts/DataChart.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Label } from "recharts";
 
 interface DataPoint {
   timestamp: string;
@@ -21,8 +21,12 @@ const DataChart = () => {
       <ResponsiveContainer width="100%" height={200}>
         <LineChart data={data}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="timestamp" />
-          <YAxis />
+          <XAxis dataKey="timestamp" tickFormatter={(tick) => new Date(tick).toLocaleTimeString()} >
+            <Label value="Time" offset={-10} position="insideBottom" />
+          </XAxis>
+          <YAxis>
+            <Label value="Value" angle={-90} position="insideLeft" style={{ textAnchor: "middle" }} />
+          </YAxis>
           <Tooltip />
           <Line type="monotone" dataKey="value" stroke="#8884d8" />
         </LineChart>
