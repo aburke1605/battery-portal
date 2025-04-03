a5b4f5e17e750366e7f054e312fdd8d43fd9b11d
aburke1605 <aodhanburke@hotmail.com>
2025-04-03 13:18:30 +0100

    use datetime only

--- website/db.py
+++ website/db.py
@@ -1,10 +1,9 @@
 import os
 from flask import Blueprint, render_template, request, jsonify
 from flask_login import login_required
-import datetime
 
 import mysql.connector
-import time
+import datetime
 
 import matplotlib.pyplot as plt
 import io
@@ -53,7 +52,7 @@ def update_db(esp_id, data):
         if abs(data["current"]) >= 0.1: # and n_rows < 10000:
             cursor.execute(f"""
                                     INSERT INTO {esp_id} (timestamp, soc, temperature, voltage, current)
-                                    VALUES (FROM_UNIXTIME({time.time()}), {data['charge']}, {data['temperature']}, {data['voltage']}, {data['current']})
+                                    VALUES ('{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}', {data['charge']}, {data['temperature']}, {data['voltage']}, {data['current']})
             """)
             DB.commit()
 
