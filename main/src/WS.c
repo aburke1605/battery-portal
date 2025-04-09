a711d03787d4f0c472ef69b08e624a46dacdc3b9
aburke1605 <aodhanburke@hotmail.com>
2025-04-09 16:18:06 +0100

    only change ESP_ID if sensible

--- main/src/WS.c
+++ main/src/WS.c
@@ -300,7 +300,7 @@ void websocket_task(void *pvParameters) {
         uint8_t two_bytes[2];
 
         read_bytes(I2C_DATA_SUBCLASS_ID, I2C_NAME_OFFSET, eleven_bytes, sizeof(eleven_bytes));
-        strncpy(ESP_ID, (char *)eleven_bytes, 10);
+        if (strcmp((char *)eleven_bytes, "") != 0) strncpy(ESP_ID, (char *)eleven_bytes, 10);
         cJSON_AddStringToObject(data, "name", ESP_ID);
 
         // read sensor data
