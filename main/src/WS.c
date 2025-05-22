696fe02f6c8cf93ca98c7a1054e7853ffe0682fd
aburke1605 <aodhanburke@hotmail.com>
2025-05-22 14:04:53 +0100

    verify ws is connected before sending registration message (I get a 'dropping message' warning here from mesh client)

--- main/src/WS.c
+++ main/src/WS.c
@@ -385,7 +385,7 @@ void websocket_event_handler(void *arg, esp_event_base_t event_base, int32_t eve
             if (VERBOSE) ESP_LOGI(TAG, "WebSocket connected");
             char websocket_connect_message[WS_MESSAGE_MAX_LEN];
             snprintf(websocket_connect_message, sizeof(websocket_connect_message), "{\"type\":\"register\",\"id\":\"%s\"}", ESP_ID);
-            send_message(websocket_connect_message);
+            if (esp_websocket_client_is_connected(ws_client)) send_message(websocket_connect_message);
             break;
 
         case WEBSOCKET_EVENT_DISCONNECTED:
