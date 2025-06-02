60f1ad8858362b23d4f4a5593a5f696afb8f9788
Yunfeng Zhao(Alex) <zyfeng90@qq.com>
2025-06-02 14:14:31 +0100

    Add default local env variables

--- website/config.py
+++ website/config.py
@@ -4,11 +4,11 @@ import os
 SECRET_KEY = '123456790'
 
 # Mysql database
-MYSQL_HOST = os.getenv("AZURE_MYSQL_HOST")
-MYSQL_PORT = os.getenv("AZURE_MYSQL_PORT")
-MYSQL_USER = os.getenv("AZURE_MYSQL_USER")
-MYSQL_PASSWORD = os.getenv("AZURE_MYSQL_PASSWORD")
-MYSQL_DATABASE = os.getenv("AZURE_MYSQL_NAME")
+MYSQL_HOST = os.getenv("AZURE_MYSQL_HOST", "localhost")
+MYSQL_PORT = os.getenv("AZURE_MYSQL_PORT", 3306)
+MYSQL_USER = os.getenv("AZURE_MYSQL_USER", "root")
+MYSQL_PASSWORD = os.getenv("AZURE_MYSQL_PASSWORD", "password")
+MYSQL_DATABASE = os.getenv("AZURE_MYSQL_NAME", "battery_data")
 
 MYSQL_SSL_CART = os.getenv("AZURE_MYSQL_SSL_CA", None),
 MYSQL_SSL_DISABLED =os.getenv("AZURE_MYSQL_SSL_DISABLED", "False") == "True"
