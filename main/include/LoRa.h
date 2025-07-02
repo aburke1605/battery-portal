7cfe889aae5e389954b70d33d0faccf13ebec2d1
aburke1605 <aodhanburke@hotmail.com>
2025-07-02 14:26:40 +0100

    add binary representation of BMS data json

--- main/include/LoRa.h
+++ main/include/LoRa.h
@@ -7,6 +7,23 @@
 
 #include <esp_err.h>
 
+typedef struct __attribute__((packed)) {
+    uint8_t type;
+    uint8_t esp_id;
+    uint8_t Q;
+    uint8_t H;
+    uint8_t V;
+    int8_t I;
+    int16_t aT;
+    int16_t iT;
+    uint16_t BL;
+    uint16_t BH;
+    uint8_t CCT;
+    uint8_t DCT;
+    int8_t CITL;
+    uint16_t CITH;
+} radio_payload;
+
 
 void lora_reset();
 
