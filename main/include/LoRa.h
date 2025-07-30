73f8224862ede77a3dc9239335bbd64f3052770c
aburke1605 <aodhanburke@hotmail.com>
2025-07-30 13:35:24 +0100

    move SPI specific stuff away from LoRa specific stuff in its own files

--- main/include/LoRa.h
+++ main/include/LoRa.h
@@ -70,14 +70,6 @@ typedef struct __attribute__((packed)) {
 #define ESC_END       0x5E  // escaped 0x7E → 0x7D 0x5E
 #define ESC_ESC       0x5D  // escaped 0x7D → 0x7D 0x5D
 
-void spi_reset();
-
-uint8_t spi_read_register(uint8_t reg);
-
-void spi_write_register(uint8_t reg, uint8_t value);
-
-esp_err_t spi_init();
-
 void lora_init();
 
 size_t json_to_binary(uint8_t* binary_message, cJSON* json_array);
