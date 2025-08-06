baf587a9702077a8a7cffce55cefd67ab19a32f5
aburke1605 <aodhanburke@hotmail.com>
2025-08-06 11:44:40 +0100

    add GPS code

---
+++ main/include/GPS.h
@@ -0,0 +1,31 @@
+#ifndef GPS_H
+#define GPS_H
+
+#include "include/config.h"
+
+typedef struct {
+    float time;
+    char status;
+    float latitude;
+    char lat_dir;
+    float longitude;
+    char long_dir;
+    float speed;
+    float course;
+    int date;
+    int magnetic_variation;
+    int magnetic_variation_dircetion;
+    char mode;
+} GPRMC;
+
+void uart_init();
+
+bool validate_nmea_checksum(const char *sentence);
+
+GPRMC* parse_gprmc(char* gprmc);
+
+GPRMC* get_gps();
+
+void gps_task(void *pvParameters);
+
+#endif
