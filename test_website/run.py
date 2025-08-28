59a1bff322441acf669335de96066c40b56d8886
aburke1605 <aodhanburke@hotmail.com>
2025-08-28 16:08:27 +0100

    untrack test_website

--- test_website/run.py
+++
@@ -1,5 +0,0 @@
-from app import create_app
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True, ssl_context=("../website/cert/local_cert.pem", "../website/cert/local_key.pem"), host="0.0.0.0")
