586e5b7d193a6a0a1d9d580c9da96af1dc10ce67
aburke1605 <aodhanburke@hotmail.com>
2025-09-29 15:20:43 +0100

    only fetch if haven't already

--- website/frontend/src/pages/Battery/BatteryDetail.tsx
+++ website/frontend/src/pages/Battery/BatteryDetail.tsx
@@ -473,7 +473,7 @@ const BatteryDetail: React.FC<BatteryDetailProps> = ({
                   {!isFromESP32 ? (
                       <div>
                         <button
-                          onClick={() => getRecommendations()}
+                          onClick={() => recommendationCards ? null : getRecommendations()}
                           className="w-full flex items-center justify-center px-4 py-2 border border-blue-300 shadow-sm text-sm font-medium rounded-md text-blue-700 bg-blue-50 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mb-4"
                         >
                           <BrainCircuit size={16} className="mr-2" />
