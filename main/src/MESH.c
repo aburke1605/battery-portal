7f2d9d67940bf3a1ba79c877485a7def8b4e03dc
aburke1605 <aodhanburke@hotmail.com>
2025-10-02 15:58:55 +0100

    only run meesh stuff if no internet connection

--- main/src/MESH.c
+++ main/src/MESH.c
@@ -19,7 +19,7 @@ static const char* TAG = "MESH";
 void connect_to_root_task(void *pvParameters) {
     while (true) {
         wifi_ap_record_t ap_info;
-        if (esp_wifi_sta_get_ap_info(&ap_info) != ESP_OK) { // if no wifi connection
+        if (esp_wifi_sta_get_ap_info(&ap_info) != ESP_OK && !connected_to_WiFi) { // if no wifi connection
             bool reconnected = false;
             
             // initial scan for other ROOT APs
@@ -113,7 +113,7 @@ void mesh_websocket_task(void *pvParameters) {
     while (true) {
         char *data_string = get_data();
 
-        if (connected_to_root && data_string != NULL && strcmp(mesh_ws_auth_token, "") != 0) {
+        if (connected_to_root && data_string != NULL && strcmp(mesh_ws_auth_token, "") != 0 && !connected_to_WiFi) {
             char uri[40+UTILS_AUTH_TOKEN_LENGTH+11];
             snprintf(uri, sizeof(uri), "ws://192.168.4.1:80/mesh_ws?auth_token=%s&esp_id=%u", mesh_ws_auth_token, ESP_ID);
             const esp_websocket_client_config_t websocket_cfg = {
@@ -223,89 +223,91 @@ esp_err_t ap_n_client_comparison_handler(esp_http_client_event_t *evt) {
 
 void merge_root_task(void *pvParameters) {
     while (true) {
-        uint8_t my_mac[6];
-        esp_wifi_get_mac(WIFI_IF_AP, my_mac);
+        if (!connected_to_WiFi) {
 
-        wifi_ap_record_t * ap_info = wifi_scan();
-        if (ap_info) {
-            ESP_LOGW(TAG, "Another ROOT AP found!!\n SSID: %s", ap_info->ssid);
+            uint8_t my_mac[6];
+            esp_wifi_get_mac(WIFI_IF_AP, my_mac);
 
-            int my_int = compare_mac(my_mac, ap_info->bssid);
+            wifi_ap_record_t * ap_info = wifi_scan();
+            if (ap_info) {
+                ESP_LOGW(TAG, "Another ROOT AP found!!\n SSID: %s", ap_info->ssid);
 
-            wifi_config_t *wifi_sta_config = malloc(sizeof(wifi_config_t));
-            memset(wifi_sta_config, 0, sizeof(wifi_config_t));
+                int my_int = compare_mac(my_mac, ap_info->bssid);
 
-            strncpy((char *)wifi_sta_config->sta.ssid, (char *)ap_info->ssid, sizeof(wifi_sta_config->sta.ssid) - 1);
-            wifi_sta_config->ap.authmode = WIFI_AUTH_OPEN;
-            ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, wifi_sta_config));
-            ESP_LOGI(TAG, "Connecting to AP... SSID: %s", wifi_sta_config->sta.ssid);
+                wifi_config_t *wifi_sta_config = malloc(sizeof(wifi_config_t));
+                memset(wifi_sta_config, 0, sizeof(wifi_config_t));
 
-            uint8_t tries = 0;
-            uint8_t max_tries = 10;
-            bool connected = false;
-            while (tries < max_tries) {
-                esp_err_t err = esp_wifi_connect();
-                if (err == ESP_OK) {
-                    ESP_LOGI(TAG, "Success, waiting for connection...");
-                    vTaskDelay(pdMS_TO_TICKS(1000));
-                    connected = true;
-                    break;
-                } else {
-                    ESP_LOGW(TAG, "Failed to connect: %s. Retrying...", esp_err_to_name(err));
-                    tries++;
-                }
-            }
+                strncpy((char *)wifi_sta_config->sta.ssid, (char *)ap_info->ssid, sizeof(wifi_sta_config->sta.ssid) - 1);
+                wifi_sta_config->ap.authmode = WIFI_AUTH_OPEN;
+                ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, wifi_sta_config));
+                ESP_LOGI(TAG, "Connecting to AP... SSID: %s", wifi_sta_config->sta.ssid);
 
-            free(wifi_sta_config);
-
-            if (connected) {
-                // change to another IP so we can communicate with other ROOT AP on it's default IP
-                esp_netif_ip_info_t ip_info;
-                if (my_int < 0) {
-                    IP4_ADDR(&ip_info.ip, 192, 168, 10, 1);
-                    IP4_ADDR(&ip_info.gw, 192, 168, 10, 1);
-                    IP4_ADDR(&ip_info.netmask, 255, 255, 255, 0);
-                    esp_netif_dhcps_stop(ap_netif);
-                    esp_netif_set_ip_info(ap_netif, &ip_info);
-                    esp_netif_dhcps_start(ap_netif);
-                    ESP_LOGI(TAG, "changed IP to 192.168.10.1");
-                    // delay to allow it to update
-                    vTaskDelay(pdMS_TO_TICKS(5000));
+                uint8_t tries = 0;
+                uint8_t max_tries = 10;
+                bool connected = false;
+                while (tries < max_tries) {
+                    esp_err_t err = esp_wifi_connect();
+                    if (err == ESP_OK) {
+                        ESP_LOGI(TAG, "Success, waiting for connection...");
+                        vTaskDelay(pdMS_TO_TICKS(1000));
+                        connected = true;
+                        break;
+                    } else {
+                        ESP_LOGW(TAG, "Failed to connect: %s. Retrying...", esp_err_to_name(err));
+                        tries++;
+                    }
                 }
 
+                free(wifi_sta_config);
 
-                // fetch the number of clients connected to the other AP
-                esp_http_client_config_t config = {
-                    .url = "http://192.168.4.1/api_num_clients",
-                    .event_handler = ap_n_client_comparison_handler,
-                };
-                esp_http_client_handle_t client = esp_http_client_init(&config);
-                esp_err_t err = esp_http_client_perform(client);
-                if (err == ESP_OK) {
-                    ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %lld",
-                            esp_http_client_get_status_code(client),
-                            esp_http_client_get_content_length(client));
-                } else {
-                    ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+                if (connected) {
+                    // change to another IP so we can communicate with other ROOT AP on it's default IP
+                    esp_netif_ip_info_t ip_info;
+                    if (my_int < 0) {
+                        IP4_ADDR(&ip_info.ip, 192, 168, 10, 1);
+                        IP4_ADDR(&ip_info.gw, 192, 168, 10, 1);
+                        IP4_ADDR(&ip_info.netmask, 255, 255, 255, 0);
+                        esp_netif_dhcps_stop(ap_netif);
+                        esp_netif_set_ip_info(ap_netif, &ip_info);
+                        esp_netif_dhcps_start(ap_netif);
+                        ESP_LOGI(TAG, "changed IP to 192.168.10.1");
+                        // delay to allow it to update
+                        vTaskDelay(pdMS_TO_TICKS(5000));
+                    }
+
+
+                    // fetch the number of clients connected to the other AP
+                    esp_http_client_config_t config = {
+                        .url = "http://192.168.4.1/api_num_clients",
+                        .event_handler = ap_n_client_comparison_handler,
+                    };
+                    esp_http_client_handle_t client = esp_http_client_init(&config);
+                    esp_err_t err = esp_http_client_perform(client);
+                    if (err == ESP_OK) {
+                        ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %lld",
+                                esp_http_client_get_status_code(client),
+                                esp_http_client_get_content_length(client));
+                    } else {
+                        ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+                    }
+                    esp_http_client_cleanup(client);
+
+
+                    if (my_int < 0) {
+                        // change back to default
+                        IP4_ADDR(&ip_info.ip, 192, 168, 4, 1);  // Change to a temporary IP
+                        IP4_ADDR(&ip_info.gw, 192, 168, 4, 1);  // Gateway IP
+                        IP4_ADDR(&ip_info.netmask, 255, 255, 255, 0);  // Subnet mask
+                        esp_netif_dhcps_stop(ap_netif);
+                        esp_netif_set_ip_info(ap_netif, &ip_info);
+                        esp_netif_dhcps_start(ap_netif);
+                        ESP_LOGI(TAG, "changed IP back to 192.168.4.1");
+                    }
+
+                    esp_wifi_disconnect();
                 }
-                esp_http_client_cleanup(client);
-
-
-                if (my_int < 0) {
-                    // change back to default
-                    IP4_ADDR(&ip_info.ip, 192, 168, 4, 1);  // Change to a temporary IP
-                    IP4_ADDR(&ip_info.gw, 192, 168, 4, 1);  // Gateway IP
-                    IP4_ADDR(&ip_info.netmask, 255, 255, 255, 0);  // Subnet mask
-                    esp_netif_dhcps_stop(ap_netif);
-                    esp_netif_set_ip_info(ap_netif, &ip_info);
-                    esp_netif_dhcps_start(ap_netif);
-                    ESP_LOGI(TAG, "changed IP back to 192.168.4.1");
-                }
-
-                esp_wifi_disconnect();
             }
         }
-
 
         vTaskDelay(pdMS_TO_TICKS(60000));
     }
