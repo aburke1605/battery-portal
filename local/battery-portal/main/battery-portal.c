c2dc28d57653015a82933fd6446c315ca9d341bf
aburke1605 <aodhanburke@hotmail.com>
2025-10-07 11:20:31 +0100

    LED stuff not used any longer

--- local/battery-portal/main/battery-portal.c
+++ local/battery-portal/main/battery-portal.c
@@ -36,18 +36,8 @@ void app_main(void) {
             unseal();
             vTaskDelay(pdMS_TO_TICKS(1000));
         }
+
         /*
-
-        // Initialize the GPIO pin as an output for LED toggling
-        gpio_config_t io_conf = {
-            .pin_bit_mask = (1ULL << I2C_LED_GPIO_PIN),
-            .mode = GPIO_MODE_OUTPUT,
-            .pull_up_en = GPIO_PULLUP_DISABLE,
-            .pull_down_en = GPIO_PULLDOWN_DISABLE,
-            .intr_type = GPIO_INTR_DISABLE
-        };
-        gpio_config(&io_conf);
-
         // grab BMS DeviceName from the BMS DataFlash
         uint8_t address[2] = {0};
         convert_uint_to_n_bytes(I2C_DEVICE_NAME_ADDR, address, sizeof(address), true);
