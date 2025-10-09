2f998376aa3d7a23ede1e8807a8c024bb4951e0c
aburke1605 <aodhanburke@hotmail.com>
2025-10-09 14:29:15 +0100

    remove testing folder

--- LINUX/components/esp_wifi_stub/src/wifi_init.c
+++
@@ -1,3 +0,0 @@
-#include <esp_wifi_types_generic.h>
-
-ESP_EVENT_DEFINE_BASE(WIFI_EVENT);
--- LINUX/main/include/DNS.h
+++
@@ -1,6 +0,0 @@
-#ifndef DNS_H
-#define DNS_H
-
-void dns_server_task(void *pvParameters);
-
-#endif // DNS_H
