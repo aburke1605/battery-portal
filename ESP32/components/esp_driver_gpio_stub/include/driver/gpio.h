cd32e52d8f10757c33128c701c787858cf7d4129
aburke1605 <aodhanburke@hotmail.com>
2025-10-30 13:08:02 +0000

    add ISR gpio to stub

--- ESP32/components/esp_driver_gpio_stub/include/driver/gpio.h
+++ ESP32/components/esp_driver_gpio_stub/include/driver/gpio.h
@@ -9,6 +9,21 @@
 extern "C" {
 #endif
 
+typedef void (*gpio_isr_t)(void *arg);
+
+typedef struct {
+    uint64_t pin_bit_mask;
+    gpio_mode_t mode;
+    gpio_pullup_t pull_up_en;
+    gpio_pulldown_t pull_down_en;
+    gpio_int_type_t intr_type;
+} gpio_config_t;
+
+static inline esp_err_t gpio_config(const gpio_config_t *pGPIOConfig) {
+    ESP_LOGI("[esp_driver_gpio_stub]", "gpio_config called");
+    return ESP_OK;
+}
+
 static inline esp_err_t gpio_set_level(gpio_num_t gpio_num, uint32_t level) {
     ESP_LOGI("[esp_driver_gpio_stub]", "gpio_set_level called");
     return ESP_OK;
@@ -21,6 +36,16 @@ static inline int gpio_get_level(gpio_num_t gpio_num) {
 
 static inline esp_err_t gpio_set_direction(gpio_num_t gpio_num, gpio_mode_t mode) {
     ESP_LOGI("[esp_driver_gpio_stub]", "gpio_set_direction called");
+    return ESP_OK;
+}
+
+static inline esp_err_t gpio_install_isr_service(int intr_alloc_flags) {
+    ESP_LOGI("[esp_driver_gpio_stub]", "gpio_install_isr_service called");
+    return ESP_OK;
+}
+
+static inline esp_err_t gpio_isr_handler_add(gpio_num_t gpio_num, gpio_isr_t isr_handler, void *args) {
+    ESP_LOGI("[esp_driver_gpio_stub]", "gpio_isr_handler_add called");
     return ESP_OK;
 }
 
--- ESP32/components/esp_driver_gpio_stub/include/driver/gpio_types.h
+++ ESP32/components/esp_driver_gpio_stub/include/driver/gpio_types.h
@@ -6,12 +6,24 @@
 extern "C" {
 #endif
 
+typedef enum {
+    GPIO_INTR_POSEDGE = 1,
+} gpio_int_type_t;
+
 #define GPIO_MODE_DEF_INPUT           (BIT0)
 #define GPIO_MODE_DEF_OUTPUT          (BIT1)
 typedef enum {
     GPIO_MODE_INPUT = GPIO_MODE_DEF_INPUT,
     GPIO_MODE_OUTPUT = GPIO_MODE_DEF_OUTPUT,
 } gpio_mode_t;
+
+typedef enum {
+    GPIO_PULLUP_DISABLE = 0x0,
+} gpio_pullup_t;
+
+typedef enum {
+    GPIO_PULLDOWN_DISABLE = 0x0,
+} gpio_pulldown_t;
 
 #ifdef __cplusplus
 }
