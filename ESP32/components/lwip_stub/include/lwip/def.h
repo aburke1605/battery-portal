32a285fe9750be4bed96253d73d9fe2ba784d7d0
aburke1605 <aodhanburke@hotmail.com>
2025-10-30 15:25:32 +0000

    fixes

--- ESP32/components/lwip_stub/include/lwip/def.h
+++ ESP32/components/lwip_stub/include/lwip/def.h
@@ -7,8 +7,8 @@ extern "C" {
 #endif
 
 #define LWIP_MAKEU32(a, b, c, d)                                               \
-  (((uint32_t)((a)&0xff) << 24) | ((uint32_t)((b)&0xff) << 16) |               \
-   ((uint32_t)((c)&0xff) << 8) | (uint32_t)((d)&0xff))
+  (((uint32_t)((a) & 0xff) << 24) | ((uint32_t)((b) & 0xff) << 16) |           \
+   ((uint32_t)((c) & 0xff) << 8) | (uint32_t)((d) & 0xff))
 
 #define PP_HTONL(x)                                                            \
   ((((x) & (uint32_t)0x000000ffUL) << 24) |                                    \
