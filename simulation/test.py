ef23f1599d795ee1de6f7e81c942e1aa4654b577
aburke1605 <aodhanburke@hotmail.com>
2025-12-01 12:57:29 +0000

    add internal resistance to gradually decrease capacity

--- simulation/test.py
+++ simulation/test.py
@@ -9,12 +9,16 @@ def simulate_data(
     I=2.0,  # Amps
     N_cycles=20,
     design_capacity=2.0,  # Amp hours
+    R0=0.05,  # ohms (internal resistance)
+    dR=0.0005,  # ohms (per cycle increase)
     dt=1.0,  # seconds
 ):
     capacity = []
 
     def OCV(SoC):
         return V_min + (V_max - V_min) * SoC
+
+    R_int = R0
 
     for cycle in range(N_cycles):
         SoC = 1.0  # start fully charged
@@ -25,7 +29,7 @@ def simulate_data(
         while True:
             # calculate new voltage
             V_OCV = OCV(SoC)
-            V = V_OCV  # - I * R_int
+            V = V_OCV - I * R_int
             if V < V_min or SoC <= 0:
                 break
 
@@ -45,6 +49,8 @@ def simulate_data(
         if cycle == 0:
             axs[0].plot(ts, Vs, marker=".")
             axs[0].plot(ts, Is, marker=".")
+
+        R_int += dR
 
     axs[1].plot(range(N_cycles), capacity, marker=".")
 
