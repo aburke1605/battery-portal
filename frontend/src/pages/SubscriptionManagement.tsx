56f0cc2e88e58bfb391bf3f07ab54623e7614e31
aburke1605 <aodhanburke@hotmail.com>
2026-01-06 12:40:50 +1300

    get user info

--- frontend/src/pages/SubscriptionManagement.tsx
+++ frontend/src/pages/SubscriptionManagement.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import { useAuth } from "../auth/AuthContext";
+
 export default function SubscriptionManagement() {
+  const { user } = useAuth();
+  const [isSubscribed, setIsSubscribed] = useState<boolean>(false);
+
   return (
     <>
-      <div>Hello</div>
+      <div>
+        Hi, {user?.first_name}.<br></br>
+        {isSubscribed ? <>Subscribed</> : <>Not subscribed</>}
+      </div>
     </>
   );
 }
