7e1f64b6737c02d0361686f60fe3b42c5e4b87a7
aburke1605 <aodhanburke@hotmail.com>
2026-01-06 16:05:36 +1300

    support expiry date in frontend

--- frontend/src/pages/SubscriptionManagement.tsx
+++ frontend/src/pages/SubscriptionManagement.tsx
@@ -3,32 +3,51 @@ import { useAuth } from "../auth/AuthContext";
 import axios from "axios";
 import apiConfig from "../apiConfig";
 
-async function getSubscriptionStatus(email: string | undefined) {
+type SubscriptionStatus = {
+  subscribed: boolean;
+  expiryDate: string | null;
+};
+
+async function getSubscriptionStatus(
+  email: string | undefined,
+): Promise<SubscriptionStatus> {
+  if (!email) return { subscribed: false, expiryDate: null };
+
   try {
-    if (email === "user@admin.dev") return true;
+    if (email === "user@admin.dev")
+      return { subscribed: true, expiryDate: "Never" };
 
     const resp = await axios.get(
       `${apiConfig.USER_API}/subscription?email=${email}`,
     );
-    if (resp.data.status == "success") return resp.data.subscribed;
-
-    return false;
+    if (resp.data.status == "success") {
+      return {
+        subscribed: resp.data.subscribed,
+        expiryDate: resp.data.expiryDate ?? null,
+      };
+    }
+    return { subscribed: false, expiryDate: null };
   } catch (error) {
     console.log("Error fetching subscription data:", error);
-    return false;
+    return { subscribed: false, expiryDate: null };
   }
 }
 
 export default function SubscriptionManagement() {
   const { user } = useAuth();
   const [isSubscribed, setIsSubscribed] = useState<boolean>(false);
+  const [expiryDate, setExpiryDate] = useState<string | null>(null);
 
   useEffect(() => {
+    if (!user?.email) return;
+
     const loadSubscriptionStatus = async () => {
-      setIsSubscribed(await getSubscriptionStatus(user?.email));
+      let subscriptionStatus = await getSubscriptionStatus(user?.email);
+      setIsSubscribed(subscriptionStatus.subscribed);
+      setExpiryDate(subscriptionStatus.expiryDate);
     };
     loadSubscriptionStatus();
-  }, []);
+  }, [user?.email]);
 
   return (
     <>
