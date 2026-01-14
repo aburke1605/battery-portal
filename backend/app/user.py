ba2f9c37d1d946c47d4ecdbca24af000bd8b44de
aburke1605 <aodhanburke@hotmail.com>
2026-01-14 17:15:38 +1300

    commented line

--- backend/app/user.py
+++ backend/app/user.py
@@ -148,7 +148,6 @@ def data():
             "first_name": current_user.first_name,
             "last_name": current_user.last_name,
             "email": current_user.email,
-            # "password": current_user.password,
             "subscribed": current_user.subscribed,
             "subscription_expiry": current_user.subscription_expiry,
         }
